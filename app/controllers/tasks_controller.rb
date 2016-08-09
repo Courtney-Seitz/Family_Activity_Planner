48fbc543be2f1d298b0627cc21627a4eef57e3aa
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-08-09 19:22:33 -0400

    three nested resource models work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,40 @@
 class TasksController < ApplicationController
 
-end
+    def new
+      @member = Member.find(params[:member_id])
+      @task = Task.new
+    end
+
+    def create
+      @member = Member.find(params[:member_id])
+      @task = @member.tasks.create!(task_params)
+      redirect_to @member
+    end
+
+    def edit
+      @task = Task.find(params[:id])
+      @member = @task.member
+    end
+
+    def update
+      @member = Member.find(params[:member_id])
+      @task = Task.find(params[:id])
+      if @task.update(task_params)
+        flash[:notice] = "Task was successfully updated."
+        redirect_to @member
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+      redirect_to @task.member
+    end
+
+    private
+    def task_params
+      params.require(:task).permit(:description, :location, :date, :note)
+    end
+  end
